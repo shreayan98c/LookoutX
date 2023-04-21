f4eb4ff5e525deff13b411c59c5b4df34d6a75cf
shreayan98c <shreayan98c@gmail.com>
2023-04-20 23:30:32 -0400

    Need to debug audio inputs

--- LookoutX/dataset.py
+++ LookoutX/dataset.py
@@ -1,7 +1,8 @@
 import cv2  # OpenCV library
 import time
+import wave
 import requests  # python http library
-# import pyaudio  # python audio library
+import pyaudio  # python audio library
 import pyttsx3  # python text to speech library
 # import speech_recognition as sr  # python speech recognition library
 import numpy as np  # python numerical library
@@ -11,16 +12,41 @@ import numpy as np  # python numerical library
 def stream(wifi_ip, port):
 
     livestream_url = f'http://{wifi_ip}:{port}/video'
+    audio_url = f'http://{wifi_ip}:{port}/audio.wav'
+    output_audio_file = 'audio_output.wav'
 
     # Initializing video
     video = cv2.VideoCapture(livestream_url)
+
+    # Open the audio stream object
+    p = pyaudio.PyAudio()
+    stream = p.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True, frames_per_buffer=1024)
+
+    # Open the output file for writing
+    output_file = wave.open(output_audio_file, 'wb')
+    output_file.setnchannels(1)
+    output_file.setsampwidth(p.get_sample_size(pyaudio.paInt16))
+    output_file.setframerate(44100)
+
+    # Make a GET request to the DroidCam app to start the audio stream
+    r = requests.get(audio_url, stream=True)
 
     # Initialize the flag variable
     flag = True
 
     while True:
+        # Read a frame from the video stream
         check, frame = video.read()
-        # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # Read a chunk of data from the audio stream
+        audio_chunk = r.raw.read(1024)
+
+        # Write the audio chunk to the output file
+        output_file.writeframes(audio_chunk)
+
+        # Convert the audio data to a numpy array and write it to the PyAudio stream
+        audio_data = np.frombuffer(audio_chunk, dtype=np.int16)
+        stream.write(audio_data)
 
         # Display the resulting frame
         if flag:
@@ -29,17 +55,22 @@ def stream(wifi_ip, port):
 
         # Waiting for 1ms
         key = cv2.waitKey(1)
-        # Break if user presses quit
-        cv2.imshow('Capturing', frame)
+        cv2.imshow('Frame', frame)
 
-        # plt.show()
-        # plt.imshow(cv2.cvtColor(test_frame, cv2.COLOR_BGR2RGB))
-        # plt.show()
-
-        if key == ord('q'):
+        # Check for the 'q' key to quit the program
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
     # Release the VideoCapture object
     video.release()
 
-    cv2.destroyAllWindows()  # Destroy all windows
+    # Destroy all windows
+    cv2.destroyAllWindows()
+
+    # Close the audio stream
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+    # Close the output file
+    output_file.close()
